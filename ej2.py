eae11195b42452a1c8c0a3ac14a964a5070a13c1
cauchasus <118222862+cauchasus@users.noreply.github.com>
2024-04-08 10:06:43 -0300

    Create ej2.py

---
+++ ej2.py
@@ -0,0 +1,9 @@
+num1 = int(input("Ingrese un numero: "))
+num2 = int(input("Ingrese un numero: "))
+
+suma = num1 + num2
+resta = num1 - num2
+resta2 = num2-num1
+
+print("Su suma es igual a: ", suma)
+print("Su resta es igual a:", resta, "|| O igual a ", resta2)
