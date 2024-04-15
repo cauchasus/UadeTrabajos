6fc3e5d58b7e54b51e0965ae3ee48b9f0f67401e
cauchasus <118222862+cauchasus@users.noreply.github.com>
2024-04-15 11:58:31 -0300

    Add files via upload

---
+++ ejercitacion5.py
@@ -0,0 +1,12 @@
+"""#Generar la serie de Fibonacci hasta el elemento 50
+Informar cuales son los valores que tienen raíz cuadrada exacta entre las posiciones 10 a 40
+Pensar como saber si un nro puede tener raíz exacta. Debatirlo con el profesor"""
+
+val_a, val_b, cont = 0, 1, 0
+while cont <= 50:
+    val_c = val_b + val_a
+    val_b = val_c
+    val_a = val_b
+    cont = cont + 1
+    if (val_a ** 0.5) * (val_a ** 0.5) == val_a and cont >= 10 and cont <= 40:
+        print("Este número tiene raiz exacta: ", val_a, "en la posición: ", cont)
