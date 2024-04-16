9d83da1aadcf40010fd0e002d11cb9037c7080ff
cauchasus <118222862+cauchasus@users.noreply.github.com>
2024-04-16 20:53:59 -0300

    Update ejercitacion5.py

--- ejercitacion5.py
+++ ejercitacion5.py
@@ -4,9 +4,8 @@ Pensar como saber si un nro puede tener raíz exacta. Debatirlo con el profesor"
 
 val_a, val_b, cont = 0, 1, 0
 while cont <= 50:
-    val_c = val_b + val_a
-    val_b = val_c
-    val_a = val_b
+    print(val_a, end=", ")
+    val_b, val_a = val_a + val_b, val_b
     cont = cont + 1
-    if (val_a ** 0.5) * (val_a ** 0.5) == val_a and cont >= 10 and cont <= 40:
+    if (val_a ** 0.5) % 1 == 0 and cont >= 10 and cont <= 40:
         print("Este número tiene raiz exacta: ", val_a, "en la posición: ", cont)
